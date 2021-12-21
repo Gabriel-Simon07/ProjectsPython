951183cda73ea2fb41330b24b068dedd5ccd4505
Gabriel Simon <gabrielsimon775@gmail.com>
2021-12-20 21:45:21 -0300

    Adicionado laço while no jogo e usando uma String interpolation

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -3,19 +3,28 @@ print("Jogo de adivinhação")
 print("--------------------")
 
 numeroSecreto = 42
+tentativas = 3
+rodada = 1
 
-numeroEntrada = input("Informe o número secreto: ")
+while(rodada <= tentativas):
 
-numeroEntradaConvertido = int(numeroEntrada)
-acertou = numeroSecreto == numeroEntradaConvertido
-numeroMaiorNumeroSecreto = numeroEntradaConvertido > numeroSecreto
+    numeroEntrada = input("Informe o número secreto: ")
 
-if(acertou) :
-    print("Você descobriu o número secreto!")
-else :
-    if numeroMaiorNumeroSecreto:
-        print("Você errou! Seu valor foi maior que o número secreto")
+    numeroEntradaConvertido = int(numeroEntrada)
+
+    acertou = numeroSecreto == numeroEntradaConvertido
+    numeroMaiorNumeroSecreto = numeroEntradaConvertido > numeroSecreto
+    numeroMenorNumeroSecreto = numeroEntradaConvertido < numeroSecreto
+
+    #print(f"{rodada}º rodada de {tentativas} tentativas")
+    #o print abaixo cóntem uma String interpolation
+    print("{} º rodada de {} tentativas".format(rodada, tentativas))
+
+    if(acertou) :
+        print("Você descobriu o número secreto!")
     else :
-        print("Você errou! Seu valor foi menor que o número secreto")
-
-print("Fim de Jogo!")
+        if numeroMaiorNumeroSecreto:
+            print("Você errou! Seu valor foi maior que o número secreto")
+        elif(numeroMenorNumeroSecreto) :
+            print("Você errou! Seu valor foi menor que o número secreto")
+    rodada = rodada +1

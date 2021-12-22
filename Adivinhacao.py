1aefa3f8b3cef1354271e26b59002370b6ddae4c
Gabriel Simon <gabrielsimon775@gmail.com>
2021-12-21 21:53:16 -0300

    Trocado o laço de repetição while pelo for e validado o número de entrada para estar entre 1 e 100

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -6,25 +6,31 @@ numeroSecreto = 42
 tentativas = 3
 rodada = 1
 
-while(rodada <= tentativas):
+for rodada in range(1, tentativas + 1) :
 
-    numeroEntrada = input("Informe o número secreto: ")
+    # print(f"{rodada}º rodada de {tentativas} tentativas")
+    # o print abaixo cóntem uma String interpolation
+    print("{} º rodada de {} tentativas".format(rodada, tentativas))
+
+    numeroEntrada = input("Informe um número secreto entre 1 e 100: ")
 
     numeroEntradaConvertido = int(numeroEntrada)
 
-    acertou = numeroSecreto == numeroEntradaConvertido
-    numeroMaiorNumeroSecreto = numeroEntradaConvertido > numeroSecreto
-    numeroMenorNumeroSecreto = numeroEntradaConvertido < numeroSecreto
+    if(numeroEntradaConvertido < 1 or numeroEntradaConvertido > 100) :
+        print("O número secreto informado é inválido, informe novamente")
+        continue
 
-    #print(f"{rodada}º rodada de {tentativas} tentativas")
-    #o print abaixo cóntem uma String interpolation
-    print("{} º rodada de {} tentativas".format(rodada, tentativas))
+        acertou = numeroSecreto == numeroEntradaConvertido
+        numeroMaiorNumeroSecreto = numeroEntradaConvertido > numeroSecreto
+        numeroMenorNumeroSecreto = numeroEntradaConvertido < numeroSecreto
 
-    if(acertou) :
-        print("Você descobriu o número secreto!")
-    else :
-        if numeroMaiorNumeroSecreto:
-            print("Você errou! Seu valor foi maior que o número secreto")
-        elif(numeroMenorNumeroSecreto) :
-            print("Você errou! Seu valor foi menor que o número secreto")
-    rodada = rodada +1
+        if(acertou) :
+            print("Você descobriu o número secreto!")
+            break
+        else :
+            if numeroMaiorNumeroSecreto:
+                print("Você errou! Seu valor foi maior que o número secreto")
+            elif(numeroMenorNumeroSecreto) :
+                print("Você errou! Seu valor foi menor que o número secreto")
+    print("Fim de jogo!")
+

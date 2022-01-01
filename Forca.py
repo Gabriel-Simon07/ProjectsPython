a2b67b7c35c32d41e977d2afc9de1eec7710215f
Gabriel Simon <gabrielsimon775@gmail.com>
2022-01-01 02:39:38 -0300

    Adicionado arquivo contendo as palavras secretas

--- Forca.py
+++ Forca.py
@@ -1,25 +1,51 @@
+import random
+
 def jogar():
     print("--------------------")
     print("Jogo da forca")
     print("--------------------")
 
-    palavraSecreta = "batata"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    arquivo = open("palavras.txt", "r")
+
+    listaPalavras = []
+    for linha in arquivo:
+        linha =  linha.strip()
+        listaPalavras.append(linha)
+
+    arquivo.close()
+
+    posicaoPalavraSecreta = random.randrange(0,len(listaPalavras))
+
+    palavraSecreta = listaPalavras[posicaoPalavraSecreta].upper()
+    letras_acertadas = ["_" for letra in palavraSecreta]
+
     enforcou = False
     acertou = False
+    qtdErros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
-        entrada = input("Informe a letra: ").strip()
+        entrada = input("Informe a letra: ").strip().upper()
 
-        posicao = 0
-        for letra in palavraSecreta:
-            if (entrada.upper() == letra.upper()):
-                letras_acertadas[posicao] = letra
-            posicao += 1
+        if(entrada in palavraSecreta):
+            posicao = 0
+            for letra in palavraSecreta:
+                if entrada == letra:
+                    letras_acertadas[posicao] = letra
+                posicao += 1
+        else:
+            qtdErros+=1
+
+        enforcou = qtdErros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+
+    if acertou:
+        print("Você acertou a palavra secreta!")
+    else:
+        print("Você não conseguiu acertar a palavra secreta!")
     print("Fim de jogo!")
 
 if(__name__ == "__main__"):
